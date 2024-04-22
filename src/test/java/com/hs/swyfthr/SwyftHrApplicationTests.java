7085a2a614680be9d8e95f2f9d165ee1e7a13bc7
Sielei Herman <hsielei@gmail.com>
2024-04-23 01:49:43 +0300

    first commit

---
+++ src/test/java/com/hs/swyfthr/SwyftHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.swyfthr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwyftHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
